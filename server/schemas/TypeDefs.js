9bdb01ca0a3125613d484b85cbd37cfcd7affb93
Justin Bedford <jbedford94@gmail.com>
2021-11-21 19:45:35 -0500

    type defs started

--- server/schemas/TypeDefs.js
+++ server/schemas/TypeDefs.js
@@ -2,7 +2,32 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+       _id: ID
+       username: String
+       email: String
+       bookCount: Int
+       savedBooks: Int
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+
+
+
+
+`;
 
 // export the typeDefs
 module.exports = typeDefs;

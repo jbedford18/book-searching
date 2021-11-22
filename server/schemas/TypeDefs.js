ed1e890c8ca7e795a45de01b3f83c6a69d311285
Justin Bedford <jbedford94@gmail.com>
2021-11-21 19:47:53 -0500

    type mutation

--- server/schemas/TypeDefs.js
+++ server/schemas/TypeDefs.js
@@ -3,6 +3,15 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+    type Query {
+       me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: bookInput): User
+        removeBook(bookId: String!): User
+    }
     type User {
        _id: ID
        username: String

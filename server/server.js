b3817fbbb3b2c11c592f02b0046485a7878d9a8c
Justin Bedford <jbedford94@gmail.com>
2021-11-22 22:01:44 -0500

    server adjust

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const db = require('./config/connection');
 const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
-
-
-/* const routes = require('./routes'); */
-const PORT = process.env.PORT || 3001;
+const db = require('./config/connection');
+const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const {typeDefs, resolvers} = require('./schemas');
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 
 
 const startServer = async () => {
@@ -37,7 +35,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+app.use(routes);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });

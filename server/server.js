ce8b59246b121522a7ac8697b3a459f93b2f29f3
Justin Bedford <jbedford94@gmail.com>
2021-11-22 19:52:51 -0500

    connect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
+const db = require('./config/connection');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
-const db = require('./config/connection');
+
 
 /* const routes = require('./routes'); */
 const PORT = process.env.PORT || 3001;
